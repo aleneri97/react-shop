bae347821e76652d1cc9b20eefce6072f872de39
Alejandro Neri <ale_neri@yahoo.com>
2022-07-14 14:02:26 -0500

    🐛 [FIX] Broken logo on Login
    * Logo styles were hiding the image
    * Webpack was not handnling the image
    * Image was being passed statically
    * Image was not being imported

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/Login.scss';
+import logo from '../../public/assets/logos/logo_bota_fora.svg';
 
-const Login = () => {
+export const Login = () => {
 	return (
 		<div className='login'>
 			<div className='form-container'>
-				<img src='./logos/logo_yard_sale.svg' alt='logo' className='logo'/>
+				<img src={logo} alt='logo' className='logo'/>
 				<h1 className='title'>Create a new password</h1>
 				<p className='subtitle'>Enter a new password for yue account</p>
 				<form action='/' className='form'>
@@ -19,5 +20,3 @@ const Login = () => {
 		</div>
 	);
 };
-
-export default Login;
--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,60 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    }
-                ]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css'
-        })
-    ],
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        // contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 3000
-    }
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	mode: 'development',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+					},
+				],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|jpg|svg|jpeg|webp)$/,
+				type: 'asset/resource',
+				generator: {
+					filename: 'assets/pictures/[hash][ext]',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		// contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 3000,
+	},
 };

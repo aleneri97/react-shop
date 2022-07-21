0d59b6833c7ac7c725fc2256503f4758b2a36459
Alejandro Neri <ale_neri@yahoo.com>
2022-07-21 18:51:48 -0500

    ✨ [ADD] Basic routes Home and NotFound
    These have no content, just placeholders

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from '../containers/Layout';
 import { Login } from '../containers/Login';
 import { RecoveryPassword } from '../containers/RecoveryPassword';
+import { Home } from '../pages/Home';
+import { NotFound } from '../pages/NotFound';
 
 import '../styles/global.scss';
 
@@ -12,8 +14,10 @@ export default function App() {
 		<BrowserRouter>
       <Layout>
         <Routes>
+          <Route exact path="/" element={ <Home /> }/>
           <Route exact path="/login" element={ <Login />  } />
           <Route exact path="/recovery" element={ <RecoveryPassword /> } />
+          <Route path="*" element={ <NotFound /> } />
         </Routes>
       </Layout>
 		</BrowserRouter>

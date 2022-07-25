be89e0b5fd587d80e0db66ff038f759d36f09cd7
Alejandro Neri <ale_neri@yahoo.com>
2022-07-25 17:14:53 -0500

    🚚 [RENAME] files of the components renamed

--- src/containers/RecoveryPasswordConfirmation.jsx
+++ src/containers/RecoveryPasswordConfirmation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/RecoveryPassword.scss';
+import '../styles/RecoveryPasswordConfirmation.scss';
 import iconEmail from '../../public/assets/icons/email.svg'
 import logo from '../../public/assets/logos/logo_bota_fora.svg'
 

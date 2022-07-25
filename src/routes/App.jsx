4dd1d0f8dc54ce9ab10fe2e3c5d4f6f7182dab18
Alejandro Neri <ale_neri@yahoo.com>
2022-07-25 17:27:11 -0500

    ✨ [ADD] ChangePassword container

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChangePassword } from '../containers/ChangePassword';
 
 import { Layout } from '../containers/Layout';
 import { Login } from '../containers/Login';
-import { RecoveryPasswordConfirmation } from '../containers/RecoveryPassword';
+import { RecoveryPasswordConfirmation } from '../containers/RecoveryPasswordConfirmation';
 import { Home } from '../pages/Home';
 import { NotFound } from '../pages/NotFound';
 
@@ -16,6 +17,7 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={ <Home /> }/>
           <Route exact path="/login" element={ <Login />  } />
+          <Route exact path="/change-password" element={ <ChangePassword /> } />
           <Route exact path="/recovery-confirmation" element={ <RecoveryPasswordConfirmation /> } />
           <Route path="*" element={ <NotFound /> } />
         </Routes>

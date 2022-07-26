574e0ad09ceb89e6343e78117e85dd24ffffba83
Alejandro Neri <ale_neri@yahoo.com>
2022-07-26 17:45:54 -0500

    🚧 [ADD] ProductList API request (WIP)
    It has not been tested

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProductItem } from '@components/ProductItem';
 import '@styles/ProductList.scss';
+import axios from 'axios'
+
+const API = 'http://api.escuelajs.co/api/v1/products';
 
 export const ProductList = () => {
+	const [products, setProducts] = useState([])
+	useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response);
+	}, []);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => {
+					<ProductItem />
+				})}
 			</div>
 		</section>
 	);

ea2bc19feaa71d3c20fc2b86929b5c66e472ff7e
Alejandro Neri <ale_neri@yahoo.com>
2022-07-26 14:14:16 -0500

    💄 [FIX-STYLE] 100vh - header height

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Header } from '../components/Header';
+import '../styles/Layout.scss';
 
 export const Layout = ({ children }) => {
 	return (
 		<div className="Layout">
 			<Header />
-			{children}
+			<div className="content">
+				{children}
+			</div>
 		</div>
 	);
 };
